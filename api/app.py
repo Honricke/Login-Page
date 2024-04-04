c466b0f6c6db7c972d29ec97497edd18b1a2d471
Henrique Bandeira <henriquebandeira307@gmail.com>
2024-04-03 22:47:06 -0300

    Atualizando Tela Login

--- api/app.py
+++ api/app.py
@@ -1,9 +1,11 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/', methods={'GET'})
+@app.route('/', methods={'POST'})
 def teste():
+    teste = request.json
+    print(teste)
     return jsonify({'status': 200})
--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,5 +1,6 @@
 import { useForm, Controller, SubmitHandler } from "react-hook-form";
 import { TextField, Button } from "@mui/material";
+import loginBG from "../assets/loginBG.png";
 
 interface FormType {
   email: string;
@@ -11,51 +12,84 @@ function Login() {
   const onSubmit: SubmitHandler<FormType> = (data) => console.log(data);
 
   return (
-    <form
-      onSubmit={handleSubmit(onSubmit)}
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        // alignItems: 'flex-start',
-        width: "400px",
-        gap: "20px",
-      }}
-    >
-      <Controller
-        control={control}
-        name="email"
-        defaultValue=""
-        render={({ field: { value, onChange } }) => (
-          <TextField
-            id="inputEmail"
-            value={value}
-            onChange={onChange}
-            label="Email"
-            variant="outlined"
-            type="email"
+    <div style={{ display: "flex" }}>
+      <div
+        style={{
+          // width: "60%",
+          height: "100vh",
+          backgroundImage: `url(${loginBG})`,
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+          flex: "1 10 60%",
+        }}
+      ></div>
+      <div style={{ display: "flex", alignItems: "center", flex: "1 1 auto" }}>
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          style={{
+            display: "grid",
+            width: "100%",
+            minWidth: "280px",
+            maxWidth: "450px",
+            gap: "20px",
+            margin: "0 auto",
+            padding: "3rem",
+            boxSizing: "border-box",
+            flexGrow: 1,
+          }}
+        >
+          <h2 style={{ color: "#434343", marginTop: 0 }}>Login</h2>
+          <Controller
+            control={control}
+            name="email"
+            defaultValue=""
+            render={({ field: { value, onChange } }) => (
+              <TextField
+                id="inputEmail"
+                value={value}
+                onChange={onChange}
+                label="Email"
+                variant="outlined"
+                type="email"
+              />
+            )}
           />
-        )}
-      />
-      <Controller
-        control={control}
-        name="pass"
-        defaultValue=""
-        render={({ field: { value, onChange } }) => (
-          <TextField
-            id="inputEmail"
-            value={value}
-            onChange={onChange}
-            label="Password"
-            variant="outlined"
-            type="password"
+          <Controller
+            control={control}
+            name="pass"
+            defaultValue=""
+            render={({ field: { value, onChange } }) => (
+              <TextField
+                id="inputEmail"
+                value={value}
+                onChange={onChange}
+                label="Password"
+                variant="outlined"
+                type="password"
+              />
+            )}
           />
-        )}
-      />
-      <Button type="submit" variant="contained" style={{ outline: "None" }}>
-        Send
-      </Button>
-      <a href="">Esqueci a senha</a>
-    </form>
+          <Button type="submit" variant="contained" style={{ outline: "None" }}>
+            Login
+          </Button>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "space-between",
+              gap: "20px",
+            }}
+          >
+            <a style={{ fontSize: "0.8em" }} href="">
+              Forgot Password?
+            </a>
+            <p style={{ fontSize: "0.8em", color: "#777", margin: 0 }}>
+              Don't have an account? <a href="">Sign Up</a>
+            </p>
+          </div>
+        </form>
+      </div>
+    </div>
   );
 }
 
